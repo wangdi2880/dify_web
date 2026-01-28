ccc993cd5e93c0079ea7a5b2c0d8010be30cfd9b
wangdi2880 <451753244@qq.com>
2026-01-28 10:22:55 +0800

    Add files via upload

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,1 +1,17 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import DifyView from '../views/DifyView.vue'
+import TestView from '../views/TestView.vue'
 
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/dify', component: DifyView },
+  { path: '/test', component: TestView }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
