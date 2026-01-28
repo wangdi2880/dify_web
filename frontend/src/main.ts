c4ec02b5213a3e0db5ae2efda1ebc5a6ebb1795d
wangdi2880 <451753244@qq.com>
2026-01-28 10:23:44 +0800

    Add files via upload

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from './router' // 引入路由
 import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(ElementPlus)
+app.use(router) // 使用路由
+app.mount('#app')
